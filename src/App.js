1be10eea421cfc99261b8d9d771d1bbc6f1385b3
Adam Aslan <chillcoders@gmail.com>
2020-07-18 17:29:41 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import "./App.css";
 
 function App() {
   return (
-    <SplitPane
-      split="vertical"
-      minSize={100}
-      defaultSize={parseInt(localStorage.getItem("splitPos"), 10)}
-      onChange={size => localStorage.setItem("splitPos", size)}
-    >
-      <div style={{ height: "800px", backgroundColor: "yellow" }}>
-        {" "}
-        left pane
-      </div>
-      <div style={{ height: "800px", backgroundColor: "green" }}>
-        right pane
-      </div>
-    </SplitPane>
+    <div style={{ height: "25%", backgroundColor: "green", border: "50%" }}>
+      <h1>Welcome to ZXY Gallery</h1>
+
+      <SplitPane
+        split="vertical"
+        minSize={100}
+        defaultSize={parseInt(localStorage.getItem("splitPos"), 10)}
+        onChange={size => localStorage.setItem("splitPos", size)}
+      >
+        <div style={{ height: "75%", backgroundColor: "red", border: "5%" }}>
+          <h1>This Area is Highly Toggleable</h1>
+        </div>
+        <div style={{ height: "85%", backgroundColor: "yellow", border: "5%" }}>
+          <h1>
+            We love these
+            <a target="_blank" href="http://chillcastle.com/art">
+              <h1>Artists</h1>
+            </a>
+            They have shown with us in the past.
+          </h1>
+        </div>
+      </SplitPane>
+    </div>
   );
 }
 
