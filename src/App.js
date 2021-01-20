fb0d1ce3a5e2af034c5fdbc816e1d9ac272f3568
Adam Aslan <chillcoders@gmail.com>
2021-01-20 01:45:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={{ height: "25%", backgroundColor: "green", border: "50%" }}>
-      <h1>Welcome to ZXY Gallery</h1>
+    <div style={{ height: "25%", backgroundColor: "red", border: "50%" }}>
+      <h1>Welcome to my component library</h1>
 
       <SplitPane
         split="vertical"
@@ -17,13 +17,13 @@ function App() {
         <div style={{ height: "75%", backgroundColor: "red", border: "5%" }}>
           <h1>This Area is Highly Toggleable</h1>
         </div>
-        <div style={{ height: "85%", backgroundColor: "yellow", border: "5%" }}>
+        <div style={{ height: "85%", backgroundColor: "red", border: "5%" }}>
           <h1>
             We love these
             <a target="_blank" href="http://chillcastle.com/art">
-              <h1>Artists</h1>
+              <h1>components</h1>
             </a>
-            They have shown with us in the past.
+            check em out.
           </h1>
         </div>
       </SplitPane>
